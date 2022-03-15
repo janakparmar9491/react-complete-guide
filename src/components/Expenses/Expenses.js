86f340292af952d89ab12e43fa599b3facf380d6
Janaksinh Parmar <68726506+janakparmar9491@users.noreply.github.com>
2022-03-15 14:19:17 +0530

    Section 5: Rendering Lists & Conditional Content

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,41 +2,48 @@ import React, { useState } from "react";
 
 import ExpensesFilter from "./ExpensesFilter";
 import ExpenseItem from "./ExpenseItem";
+import ExpensesChart from "./ExpensesChart";
 import Card from "../UI/Card";
 import "./Expenses.css";
 
 function Expenses(props) {
-  const [filteredYear, setFilteredYear] = useState('2020');
+  const [filteredYear, setFilteredYear] = useState("2020");
+  const [expenses, setExpenses] = useState(props.expenses);
 
-  const filterChangeHandler = selectedYear => {
+  const filterChangeHandler = (selectedYear) => {
+    const filteredExpenses = props.expenses.filter((expense) =>
+      filterDate(expense, selectedYear)
+    );
+    setExpenses(filteredExpenses);
     setFilteredYear(selectedYear);
-    console.log(selectedYear);
+  };
+
+  const filterDate = (expense, year) => {
+    if (expense.date.getFullYear().toString() === year) {
+      return expense;
+    }
   };
 
   return (
     <div>
-      <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
       <Card className="expenses">
-        <ExpenseItem
-          title={props.expenses[0].title}
-          amount={props.expenses[0].amount}
-          date={props.expenses[0].date}
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
         />
-        <ExpenseItem
-          title={props.expenses[1].title}
-          amount={props.expenses[1].amount}
-          date={props.expenses[1].date}
-        />
-        <ExpenseItem
-          title={props.expenses[2].title}
-          amount={props.expenses[2].amount}
-          date={props.expenses[2].date}
-        />
-        <ExpenseItem
-          title={props.expenses[3].title}
-          amount={props.expenses[3].amount}
-          date={props.expenses[3].date}
-        />
+        <ExpensesChart expenses={expenses} />
+        {expenses.length === 0 ? (
+          <p>No expenses found.</p>
+        ) : (
+          expenses.map((expense) => (
+            <ExpenseItem
+              key={expense.id}
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date}
+            />
+          ))
+        )}
       </Card>
     </div>
   );
